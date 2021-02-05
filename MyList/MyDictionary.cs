823718d46ed9fb52436638ae6af7715477281418
CengizhanAnar <cengizhananar@gmail.com>
2021-02-05 16:27:45 +0300

    secon

---
+++ MyList/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyList
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key,TValue value)
+        {
+            TKey[] tempKey = keys;
+            
+            keys = new TKey[tempKey.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+            }
+
+            keys[keys.Length - 1] = key;
+
+            TValue[] tempValue = values;
+            values = new TValue[tempValue.Length + 1];
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                values[i] = tempValue[i];
+            }
+            values[values.Length - 1] = value;
+
+            Console.WriteLine();
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Branş:" + keys[i] + " İsim: " + values[i]);
+            }
+        }
+
+
+
+    }
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,string> names = new MyDictionary<string,string>();
+            names.Add("Mühendis","Cengizhan");
+            names.Add("Öğretmen", "Mehmet");
+            names.Add("Doktor", "Veli");
+            names.Add("Esnaf", "Ali");
+            
+
+            names.List();
+            
+        }
+    }
+}
